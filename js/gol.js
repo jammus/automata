35108d31d807d2fa6811eb0978fbf76f4f85b4c9
jammus <jammus@gmail.com>
2013-04-21 16:23:32 +0100

    Separate drawing and updating parts.

--- js/gol.js
+++ js/gol.js
@@ -1,26 +1,20 @@
 (function() {
-    var GameOfLife = function(context, initialState) {
-        this._context = context;
-        this._state = initialState;
-        
-        this._height = this._state.length;
-        this._width = this._state[0].length;
-        
-        this._blockSize = context.canvas.width / this._height;
-    };
 
-    GameOfLife.prototype.update = function() {
-        var newState = [ ],
+    var gameOfLife = function(world) {
+        var height = world.length,
+            width = world[0].length;
+
+        var newWorld = [ ],
             row, column, cell, newCell, neighbours;
 
-        for (row = 0; row < this._height; row++) {
-            newState[row] = [ ]
+        for (row = 0; row < height; row++) {
+            newWorld[row] = [ ]
         }
 
-        for (row = 0; row < this._height; row++) {
-            for (column = 0; column < this._width; column++) {
-                var neighbours = this._countNeighbours(row, column);
-                cell = this._state[row][column];
+        for (row = 0; row < height; row++) {
+            for (column = 0; column < width; column++) {
+                var neighbours = countNeighbours(row, column);
+                cell = world[row][column];
                 newCell = cell;
                 if (cell == 0 && neighbours == 3) {
                     newCell = 1;
@@ -31,64 +25,68 @@
                 if (cell == 1 && neighbours > 3) {
                     newCell = 0;
                 }
-                newState[row][column] = newCell;
+                newWorld[row][column] = newCell;
             }
         }
 
-        this._state = newState;
+        return newWorld;
+
+        function countNeighbours(cellRow, cellColumn) {
+            var row, column, neighbours = 0;
+
+            for (row = cellRow - 1; row <= cellRow + 1; row++) {
+                for (column = cellColumn - 1; column <= cellColumn + 1; column++) {
+                    if (row == cellRow && column == cellColumn) {
+                        continue;
+                    }
+                    if (isCellLive(row, column)) {
+                        neighbours++;
+                    }
+                }
+            }
+
+            return neighbours;
+        }
+    
+        function isCellLive(row, column) {
+            if (row < 0 || row >= height) {
+                return false;
+            }
+
+            if (column < 0 || column >= width) {
+                return false;
+            }
+
+            return world[row][column] == 1;
+        }
+
     };
 
-    GameOfLife.prototype.draw = function() {
+    var drawWorld = function(context, world) {
+        var height = world.length,
+            width = world[0].length;
+
+        var blockSize = context.canvas.width / height;
+
         var row, column;
 
-        for (row = 0; row < this._height; row++) {
-            for (column = 0; column < this._width; column++) {
-                var x = column * this._blockSize,
-                    y = row * this._blockSize,
-                    cell = this._state[row][column];
+        for (row = 0; row < height; row++) {
+            for (column = 0; column < width; column++) {
+                var x = column * blockSize,
+                    y = row * blockSize,
+                    cell = world[row][column];
 
-                this._context.fillStyle = cell == 1 ? '#ff0000' : '#ffffff';
-                this._context.fillRect(x, y, this._blockSize, this._blockSize);
+                context.fillStyle = cell == 1 ? '#ff0000' : '#ffffff';
+                context.fillRect(x, y, blockSize, blockSize);
             }
         }
     };
-    
-    GameOfLife.prototype._countNeighbours = function(cellRow, cellColumn) {
-        var row, column, neighbours = 0;
 
-        for (row = cellRow - 1; row <= cellRow + 1; row++) {
-            for (column = cellColumn - 1; column <= cellColumn + 1; column++) {
-                if (row == cellRow && column == cellColumn) {
-                    continue;
-                }
-                if (this._isCellLive(row, column)) {
-                    neighbours++;
-                }
-            }
-        }
-
-        return neighbours;
-    };
-    
-    GameOfLife.prototype._isCellLive = function(row, column) {
-        if (row < 0 || row >= this._height) {
-            return false;
-        }
-
-        if (column < 0 || column >= this._width) {
-            return false;
-        }
-
-        return this._state[row][column] == 1;
-    };
-    
     window.addEventListener('load', function() {
         var canvas = document.getElementById('board'),
             context = canvas.getContext("2d");
         
-        var game = new GameOfLife(
-            context,
-            [
+        var world = [
                 [ 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 ],
                 [ 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 ],
                 [ 0, 1, 1, 1, 0, 0, 0, 0, 0, 0 ],
@@ -99,14 +97,13 @@
                 [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
                 [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
                 [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
-            ]
-        );
+            ];
         
-        game.draw();
+        drawWorld(context, world);
         setTimeout(tick, 200);
         function tick() {
-            game.update();
-            game.draw();
+            world = gameOfLife(world);
+            drawWorld(context, world);
             setTimeout(tick, 200);
         }
     });
